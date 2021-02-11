b40f960b37e2b64429c883bfd8b09a792a58c9ee
tod619 <trevorodwyer619@hotmail.com>
2021-02-11 14:40:44 +0000

    add background colors to nav-links using js in script.js file

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// add background colors to navigation links
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee']
+
+let i = 0;
+
+Array.from(document.querySelectorAll('.nav-link')).forEach((item) => {
+    item.style.cssText = `background-color: ${colors[i++]} `
+    
+})
+
 // card navigation buttons to flip between front and back sides 
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
     item.onclick = () => {
